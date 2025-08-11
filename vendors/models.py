b219d2ffc2cbf5d68a4d3cc2e922505fbfac5f5e
PeymanNr <peyman.naseri7@gmail.com>
2025-08-11 18:30:32 +0330

    refactor Vendor model to inherit from BaseModel and remove timestamp fields

--- vendors/models.py
+++ vendors/models.py
@@ -1,17 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Q, CheckConstraint
+from utils.base_models import BaseModel
 
 
-class TimeStampedModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
-
-
-class Vendor(TimeStampedModel):
+class Vendor(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='vendor_profile')
     name = models.CharField(max_length=255, verbose_name="Vendor Name", db_index=True)
     balance = models.DecimalField(max_digits=18, decimal_places=2, default=0)
